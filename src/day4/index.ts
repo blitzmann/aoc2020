06d673f3f4eeff321d21e59b193fb653c143dc7d
blitzmann <holmes.ryan.90@gmail.com>
2020-12-04 09:31:30 -0500

    Clean up  Day 4, create a Passport class to house the logic

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -3,100 +3,84 @@ import { test, readInput } from "../utils/index"
 const prepareInput = (rawInput: string) => rawInput
 
 const input = prepareInput(readInput())
-let reqFields = new Set([
-  'byr',
-  'iyr',
-  'eyr',
-  'hgt',
-  'hcl',
-  'ecl',
-  'pid',
-  // 'cid',
-])
-const goA = (input) => {
-  let passports = input.split("\n\n");
-  let valid = 0
-  for (let passport of passports) {
-    let data = passport.trim().split(/[\n\r ]/)
-    let fields = new Set(data.map(x => x.split(":")[0]))
 
-    let difference = new Set(
-      [...reqFields].filter(x => !fields.has(x))).size;
+class Passport {
+    amb
+    blu
+    brn
+    gry
+    grn
+    hzl
+    oth
+    cid
 
-    if (difference === 0) { valid += 1 }
-  }
+    private validEcl = new Set([
+        "amb",
+        "blu",
+        "brn",
+        "gry",
+        "grn",
+        "hzl",
+        "oth"
+    ])
 
-  return valid
+    private hgtValidators = new Map([
+        ['cm', (value) => +value >= 150 && +value <= 193],
+        ["in", (value) => +value >= 59 && +value <= 76],
+    ])
+
+    private validators = new Map([
+        ['byr', (value) => +value >= 1290 && +value <= 2002 && value.toString().length === 4],
+        ['iyr', (value) => +value >= 2010 && +value <= 2020 && value.toString().length === 4],
+        ['eyr', (value) => +value >= 2020 && +value <= 2030 && value.toString().length === 4],
+        ['hgt', (value) => {
+            let data = value.split(/(\d+)(cm|in)/).filter(x => x !== "")
+            let func = this.hgtValidators.get(data[1])
+            return data.length === 2 && func && func(data[0])
+        }],
+        ['hcl', (value) => value.length === 7 && !!value.match(/#[\d|a-z]{6}/)],
+        ['ecl', (value) => this.validEcl.has(value)],
+        ['pid', (value) => value.length === 9 && !!value.match(/[\d]{9}/)]
+    ])
+
+    constructor(rawString: string) {
+        let data = rawString.trim().split(/[\n\r ]/)
+        for (let [key, value] of data.map(x => x.split(":"))) {
+            this[key] = value
+        }
+    }
+
+    get hasAllReqFields() {
+        for (let req of this.validators.keys()) {
+            if (!this[req]) {
+                return false
+            }
+        }
+        return true
+    }
+
+    get allReqFieldsValid() {
+        for (let key of this.validators.keys()) {
+            if (!this.validators.get(key)(this[key])) {
+                return false
+            }
+        }
+        return true;
+    }
+
+    get isValid() {
+        return this.hasAllReqFields && this.allReqFieldsValid
+    }
 }
 
-let validation = new Map([
-  ['byr', (value) => {
-    return +value >= 1290 && +value <= 2002 && value.toString().length === 4
-  }],
-  ['iyr', (value) => {
-    return +value >= 2010 && +value <= 2020 && value.toString().length === 4
-  }],
-  ['eyr', (value) => {
-    return +value >= 2020 && +value <= 2030 && value.toString().length === 4
-  }],
-  ['hgt', (value) => {
-    let data = value.split(/(\d+)(cm|in)/).filter(x => x !== "")
-    if (data.length !== 2) {
-      return false
-    }
-    let [num, unit] = data
-    if (unit === "cm") {
-      return +num >= 150 && +num <= 193
-    }
-    else if (unit === "in") {
-      return +num >= 59 && +num <= 76
-    }
-  }],
-  ['hcl', (value) => {
-    return value.length === 7 && !!value.match(/#[\d|a-z]{6}/)
-  }],
-  ['ecl', (value) => {
-    let valid = new Set([
-      "amb",
-      "blu",
-      "brn",
-      "gry",
-      "grn",
-      "hzl",
-      "oth"])
-    return valid.has(value)
-  }],
-  ['pid', (value) => {
-    return value.length === 9 && !!value.match(/[\d]{9}/)
-  }]])
+const goA = (input) => {
+    let passports = input.split("\n\n");
+    return passports.map(x => new Passport(x)).filter(x => x.hasAllReqFields).length
+}
 
 const goB = (input) => {
-  let passports = input.split("\n\n");
-  let valid = 0
-  for (let passport of passports) {
-    let data = passport.trim().split(/[\n\r ]/)
-
-    let fields = new Set(data.map(x => x.split(":")[0]))
-    let validity = data.map(x => {
-      let [key, value] = x.split(":")
-
-      let func = validation.get(key)
-      if (!func) {
-        return null
-      }
-
-      return func(value)
-    })
-    let difference = new Set(
-      [...reqFields].filter(x => !fields.has(x))).size;
-    console.log(fields, validity)
-    if (difference === 0 && validity.filter(x => x !== null).every(Boolean)) {
-      // check values  
-      valid += 1
-    }
-  }
-
-  return valid
+    let passports = input.split("\n\n");
+    return passports.map(x => new Passport(x)).filter(x => x.isValid).length
 }
 
 /* Tests */
