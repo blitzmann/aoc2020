c656620c62bcc653cafaf7ce6c1e2ea323827125
blitzmann <holmes.ryan.90@gmail.com>
2020-12-06 00:23:36 -0500

    Day 6 clean up

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -6,40 +6,40 @@ const prepareInput = (rawInput: string) => rawInput
 const input = prepareInput(readInput())
 
 const goA = (input) => {
-  let groups: string[] = input.split("\n\n")
-  let groupAnswers = [];
-  for (let group of groups){
-    let answers = new Set();
-    let persons = group.split("\n");
-    for (let person of persons){
-      for (let x of person.split("")){
-        answers.add(x)
-      }
-    }
-    groupAnswers.push(answers.size);
-  }
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  return groupAnswers.reduce(reducer)
-  return 
+    let groups: string[] = input.split("\n\n")
+
+    let groupAnswers = groups.map(group=>{
+        // we will store all answers that have at least one 'yes' in an answers set
+        let answers = new Set();
+        for (let person of group.split("\n")) {
+            for (let x of person.split("")) {
+                answers.add(x)
+            }
+        }
+        return answers.size
+    })
+
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return groupAnswers.reduce(reducer)
 }
 
 const goB = (input) => {
-  let groups: string[] = input.split("\n\n")
-  let groupAnswers = [];
-  for (let group of groups){
-    let answers = new Set('abcdefghijklmnopqrstuvwxyz'.split(''));
-    let persons = group.split("\n");
-    for (let person of persons){
-      answers = new Set(
-        [...answers].filter(x => new Set(person.split("")).has(x)))
-    }
+    let groups: string[] = input.split("\n\n")
+    let groupAnswers = groups.map(group => {
+        // for part two, we want to only take the intersection of all people in the group.
+        // Start with the full question list
+        let answers = new Set('abcdefghijklmnopqrstuvwxyz'.split(''));
+        for (let person of group.split("\n")) {
+            // mutate answers as the intersection of this person and the previous answers
+            answers = new Set(
+                [...answers].filter(x => new Set(person.split("")).has(x)))
+        }
 
-    groupAnswers.push(answers.size)
+        return answers.size
+    });
 
-  }
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  return groupAnswers.reduce(reducer)
-  return 
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return groupAnswers.reduce(reducer)
 }
 
 /* Tests */
