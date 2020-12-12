02ce8147330d0db6037730812cbc0812f395b996
blitzmann <holmes.ryan.90@gmail.com>
2020-12-12 01:04:03 -0500

    Clean up day 12

--- src/day12/index.ts
+++ src/day12/index.ts
@@ -5,180 +5,126 @@ const prepareInput = (rawInput: string) => rawInput
 const input = prepareInput(readInput())
 
 
-let map = new Map([
+enum CONST {
+  FORWARD = "F",
+  RIGHT = "R",
+  LEFT = "L",
+  NORTH = "N",
+  SOUTH = "S",
+  WEST = "W",
+  EAST = "E",
+}
+
+let directionTurn = new Map([
   // dir      left     right
-  ['east', ['north', 'south']],
-  ['north', ['west', 'east']],
-  ['south', ['east', 'west']],
-  ['west', ['south', 'north']],
+  [CONST.EAST, [CONST.NORTH, CONST.SOUTH]],
+  [CONST.NORTH, [CONST.WEST, CONST.EAST]],
+  [CONST.SOUTH, [CONST.EAST, CONST.WEST]],
+  [CONST.WEST, [CONST.SOUTH, CONST.NORTH]],
 ])
 
-let map2 = new Map([
-  // dir
-  ['east', [1, null]],
-  ['north', [null, 1]],
-  ['south', [null, -1]],
-  ['west', [-1, null]],
+let directionMod = new Map([
+  [CONST.EAST, [1, null]],
+  [CONST.NORTH, [null, 1]],
+  [CONST.SOUTH, [null, -1]],
+  [CONST.WEST, [-1, null]],
 ])
+
 // x, y
 
 const goA = (input) => {
   input = input.split("\n").map(x => {
     return x.split(/([A-Z])(\d+)/).filter(x => x !== "")
   })
-  let direction = 'east'
+
+  let direction = CONST.EAST
   let coord = [0, 0]
+
   for (let ins of input) {
-
-    if (ins[0] === "F") {
-      let [modx, mody] = map2.get(direction)
-      if (modx) {
-        coord[0] += (modx * ins[1])
+    switch (ins[0]) {
+      case CONST.FORWARD: {
+        let [modx, mody] = directionMod.get(direction)
+        if (modx) {
+          coord[0] += (modx * ins[1])
+        }
+        if (mody) {
+          coord[1] += (mody * ins[1])
+        }
+        break;
       }
-      if (mody) {
-        coord[1] += (mody * ins[1])
+      case CONST.RIGHT:
+      case CONST.LEFT: {
+        let num = +ins[1] / 90
+        for (let x = 0; x < num; x++) {
+          direction = directionTurn.get(direction)[ins[0] === CONST.RIGHT ? 1 : 0]
+        }
+        break;
       }
-    }
-    if (ins[0] === "R") {
-      let num = +ins[1] / 90
-      for (let x = 0; x < num; x++) {
-        direction = map.get(direction)[1]
-      }
-    }
-    if (ins[0] === "L") {
-      let num = +ins[1] / 90
-      for (let x = 0; x < num; x++) {
-        direction = map.get(direction)[0]
-      }
-    }
-    if (ins[0] === "N") {
-      let [modx, mody] = map2.get('north')
-      if (modx) {
-        coord[0] += (modx * ins[1])
-      }
-      if (mody) {
-        coord[1] += (mody * ins[1])
-      }
-    }
-    if (ins[0] === "S") {
-      let [modx, mody] = map2.get('south')
-      if (modx) {
-        coord[0] += (modx * ins[1])
-      }
-      if (mody) {
-        coord[1] += (mody * ins[1])
+      case CONST.NORTH:
+      case CONST.SOUTH:
+      case CONST.EAST:
+      case CONST.WEST: {
+        let [modx, mody] = directionMod.get(ins[0])
+        if (modx) {
+          coord[0] += (modx * ins[1])
+        }
+        if (mody) {
+          coord[1] += (mody * ins[1])
+        }
+        break;
       }
     }
-    if (ins[0] === "E") {
-      let [modx, mody] = map2.get('east')
-      if (modx) {
-        coord[0] += (modx * ins[1])
-      }
-      if (mody) {
-        coord[1] += (mody * ins[1])
-      }
-    }
-    if (ins[0] === "W") {
-      let [modx, mody] = map2.get('west')
-      if (modx) {
-        coord[0] += (modx * ins[1])
-      }
-      if (mody) {
-        coord[1] += (mody * ins[1])
-      }
-    }
-
-    console.log(direction, coord)
-
   }
-  console.log(coord)
+
   return Math.abs(coord[0]) + Math.abs(coord[1])
 }
 
 const goB = (input) => {
-  console.log("PART 2")
   input = input.split("\n").map(x => {
     return x.split(/([A-Z])(\d+)/).filter(x => x !== "")
   })
-  let direction = 'east'
+
   let waypointRel = [10, 1]
   let coord = [0, 0]
+
   for (let ins of input) {
+    switch (ins[0]) {
+      case CONST.FORWARD:
+        coord[0] += ins[1] * waypointRel[0]
+        coord[1] += ins[1] * waypointRel[1]
+        break;
+      case CONST.RIGHT: {
+        let num = +ins[1] / 90
+        for (let x = 0; x < num; x++) {
+          waypointRel = [waypointRel[1], -1 * waypointRel[0]]
+        }
+        break;
+      }
+      case CONST.LEFT: {
+        let num = +ins[1] / 90
+        for (let x = 0; x < num; x++) {
+          waypointRel = [waypointRel[1] * -1, waypointRel[0]]
+        }
+        break
+      }
+      case CONST.NORTH:
+      case CONST.SOUTH:
+      case CONST.EAST:
+      case CONST.WEST: {
+        let [modx, mody] = directionMod.get(ins[0])
+        if (modx) {
+          waypointRel[0] += (modx * ins[1])
+        }
+        if (mody) {
+          waypointRel[1] += (mody * ins[1])
+        }
+        break;
+      }
 
-    if (ins[0] === "F") {
-      let [modx, mody] = map2.get(direction)
-      
-        coord[0] += (modx||1) * ins[1] * waypointRel[0]
-      
-
-        coord[1] += (mody||1) * ins[1] * waypointRel[1]
-      
     }
-    if (ins[0] === "R") {
-      let num = +ins[1] / 90
-      for (let x = 0; x < num; x++) {
-        // 10 east, 4 north
-        // 4 east, -10 north [0] = [1], [1] = NEG [1]
-        // -10 east, -4 north [0] = 1, [1] = NEG[0]
-        // -4 east, 10 north
-
-        waypointRel = [waypointRel[1], -1* waypointRel[0]]
-      }
-    }
-    if (ins[0] === "L") {
-      let num = +ins[1] / 90
-      for (let x = 0; x < num; x++) {
-        // 4 east,  2 north
-        //  -2 east, 4 north [0] = NEG [1], [1] = [0]
-        // -4 east, -2 north [0] = NEG [1], [1] = [0]
-        // 2 east, -4 north
-        waypointRel = [waypointRel[1]*-1, waypointRel[0]]
-      }
-    }
-    if (ins[0] === "N") {
-      let [modx, mody] = map2.get('north')
-      if (modx) {
-        waypointRel[0] += (modx * ins[1])
-      }
-      if (mody) {
-        waypointRel[1] += (mody * ins[1])
-      }
-    }
-    if (ins[0] === "S") {
-      let [modx, mody] = map2.get('south')
-      if (modx) {
-        waypointRel[0] += (modx * ins[1])
-      }
-      if (mody) {
-        waypointRel[1] += (mody * ins[1])
-      }
-    }
-    if (ins[0] === "E") {
-      let [modx, mody] = map2.get('east')
-      // [-1, null]
-      if (modx) {
-        waypointRel[0] += (modx * ins[1])
-      }
-      if (mody) {
-        waypointRel[1] += (mody * ins[1])
-      }
-    }
-    if (ins[0] === "W") {
-      let [modx, mody] = map2.get('west')
-      if (modx) {
-        waypointRel[0] += (modx * ins[1])
-      }
-      if (mody) {
-        waypointRel[1] += (mody * ins[1])
-      }
-    }
-
-    console.log(ins, coord, waypointRel)
-
   }
-  console.log(coord)
-  return Math.abs(coord[0]) + Math.abs(coord[1])
 
+  return Math.abs(coord[0]) + Math.abs(coord[1])
 }
 
 /* Tests */
