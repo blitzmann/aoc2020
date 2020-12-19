e46f368f7d768b552e8cfeb9ecb4b6abf0a6064f
blitzmann <holmes.ryan.90@gmail.com>
2020-12-18 19:21:27 -0500

    Day 18, part 2

--- src/day18/index.ts
+++ src/day18/index.ts
@@ -5,12 +5,10 @@ const prepareInput = (rawInput: string) => rawInput
 const input = prepareInput(readInput())
 
 const traverse = (inner: string[], mode = false) => {
-  console.log("--- starting traverse")
   let leftSide = ""
   let rightSide = ""
   let op
   for (let i = 0; i < inner.length; i++) {
-    console.log(`position: ${i}`, inner.slice(i).join(""))
     let char = inner[i]
 
     switch (char) {
@@ -29,7 +27,6 @@ const traverse = (inner: string[], mode = false) => {
         }
         break;
       case "(":
-        console.log("starting parenths at i = ", i)
         let [i2, result] = traverse(inner.slice(i + 1))
         i += i2 + 1
         if (leftSide) {
@@ -37,11 +34,9 @@ const traverse = (inner: string[], mode = false) => {
         } else {
           leftSide = result.toString()
         }
-        console.log(`pos: ${i} left: ${leftSide} right: ${rightSide}`)
         break
       case ")":
         leftSide = op(Number(leftSide), Number(rightSide))
-        console.log("found a ) at i = ", i, leftSide)
         rightSide = ""
         op = null
         return [i, leftSide]
@@ -53,76 +48,39 @@ const traverse = (inner: string[], mode = false) => {
         break
     }
   }
-  console.log("--- ending traverse")
   return op(Number(leftSide), Number(rightSide))
 }
 
 
-const traverse2 = (inner: string[], mode = false) => {
-  console.log("--- starting traverse")
-  let leftSide = ""
-  let rightSide = ""
-  let op
-  let mulGroup = []
-  for (let i = 0; i < inner.length; i++) {
-    console.log(`position: ${i}`, inner.slice(i).join(""))
+const traverseGroup = (inner: string[], mode = false): [number, number] => {
+  let i = 0
+  let acc = ""
+  loop: for (; i < inner.length; i++) {
     let char = inner[i]
 
     switch (char) {
-      case "+":
-        if (op) {
-          leftSide = op(Number(leftSide), Number(rightSide))
-          rightSide = ""
-          op = null
-        }
-        op = (a, b) => (a + b);
-        break
-      case "*":
-        if (op) {
-          leftSide = op(Number(leftSide), Number(rightSide))
-          console.log("multiply hit, op: ", leftSide)
-          rightSide = ""
-          op = null
-        }
-        mulGroup.push(leftSide)
-        leftSide = ""
-        break;
       case "(":
-        console.log("starting parenths at i = ", i)
-        let [i2, result] = traverse2(inner.slice(i + 1))
+        let [i2, result] = traverseGroup(inner.slice(i + 1))
         i += i2 + 1
-        console.log (result)
-          
-          if (op) {
-            rightSide = result.toString()
-          } else { 
-            mulGroup.push(result)
-          }
-        console.log(`pos: ${i} left: ${leftSide}, right: ${rightSide}`)
+        acc += result.toString()
         break
       case ")":
-        
-        leftSide = op(Number(leftSide), Number(rightSide))
-        mulGroup.push(leftSide)
-        leftSide = ""
-        console.log(mulGroup, leftSide, rightSide)
-        console.log("found a ) at i = ", i, leftSide)
-        rightSide = ""
-        op = null
-        return [i, mulGroup.reduce((a,b)=>a*b, 1)]
-        break
+        break loop;
       default:
-        if (op) {
-          rightSide += char
-        } else { 
-          leftSide += char 
-        }
+        acc += char
         break
     }
   }
-  console.log("--- ending traverse")
-  console.log(op)
-  return op ? op(Number(leftSide), Number(rightSide)) : mulGroup.reduce((a,b)=>a*b, 1) : 
+
+  let result = acc
+    .split("*")
+    .map(x => x
+            .split("+")
+            .map(Number)
+            .reduce((a, b) => a + b), 0
+    )
+    .reduce((a, b) => a * b, 1)
+  return [i, result]
 }
 
 const goA = (input) => {
@@ -132,15 +90,16 @@ const goA = (input) => {
     .map(x => x.replace(/ /g, "").trim().split(""))
     .map(traverse)
     .reduce((a, b) => a + b, 0)
-}
-
-const goB = (input) => {
-  return input
-  .trim()
-  .split("\n")
-  .map(x => x.replace(/ /g, "").trim().split(""))
-  .map(traverse2)
-  .reduce((a, b) => a + b, 0)
+  }
+  
+  const goB = (input) => {
+    return input
+    .trim()
+    .split("\n")
+    .map(x => x.replace(/ /g, "").trim().split(""))
+    .map(traverseGroup)
+    .map(x=>x[1])
+    .reduce((a, b) => a + b, 0)
 }
 
 /* Tests */
@@ -150,9 +109,9 @@ const goB = (input) => {
 /* Results */
 
 console.time("Time")
-// const resultA = goA(input)
+const resultA = goA(input)
 const resultB = goB(input)
 console.timeEnd("Time")
 
-// console.log("Solution to part 1:", resultA)
+console.log("Solution to part 1:", resultA)
 console.log("Solution to part 2:", resultB)
